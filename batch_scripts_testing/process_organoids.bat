5990377c0429041397539f4a277b2322fd48646c
FLIMinator <7783135+FLIMinator@users.noreply.github.com>
2023-04-11 18:49:20 +0100

    all classes in single py file
    no longer classes in main scripts that use GUI interface

--- batch_scripts_testing/process_organoids.bat
+++
@@ -1,5 +0,0 @@
-REM C:\Fiji.app\ImageJ-win64.exe --ij2 --headless --console --run "C:\Users\user\Documents\GitHub\dOPM_Characterisation\scripts\dOPM_crick_scripts\Colin_scripts\Expt_10_scripts\process_organoids_day_1.py"
-
-C:\Fiji.app\ImageJ-win64.exe --ij2 --headless --console --run "C:\Users\user\Documents\GitHub\dOPM_Characterisation\scripts\dOPM_crick_scripts\Colin_scripts\Expt_10_scripts\process_organoids_day_2.py"
-
-REM C:\Fiji.app\ImageJ-win64.exe --ij2 --headless --console --run "C:\Users\user\Documents\GitHub\dOPM_Characterisation\scripts\dOPM_crick_scripts\Colin_scripts\Expt_10_scripts\process_organoids_day_3.py"
